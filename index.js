b81389aa083421b7b7e8992aa0b4208f8433f1fd
gyeong0212 <gyeonglim0212@gmail.com>
2023-09-20 15:40:03 +0900

    add formatter

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3000;
+const http = require("http");
+
+const hostname = "127.0.0.1";
+const port = 3300;
+
 const server = http.createServer((req, res) => {
-res.statusCode = 200;
-res.setHeader('Content-Type', 'text/plain');
-res.end('Hello world\n');
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("rururururu\n");
 });
+
 server.listen(port, hostname, () => {
-console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
-f
